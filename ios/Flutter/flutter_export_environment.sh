2b0b39704a1a43df6fb5a2ee61865ebd3cadf9da
meetpatel83 <129854355+meetpatel83@users.noreply.github.com>
2025-05-02 10:41:15 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kreonsolutions/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kreonsolutions/AndroidStudioProjects/ClipCut"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/kreonsolutions/AndroidStudioProjects/ClipCut/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/kreonsolutions/AndroidStudioProjects/ClipCut/.dart_tool/package_config.json"
